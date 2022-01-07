03ec5fbd74942799119762531bdb9e56cb43bf6c
Razafiarison Tafita <tafita.raza@outlook.com>
2022-01-07 21:43:33 +0300

    adding vue router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
 
 import 'bootstrap/dist/js/bootstrap.js'
+import router from './router'
