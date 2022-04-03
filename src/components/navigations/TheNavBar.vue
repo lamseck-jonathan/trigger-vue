35d570942c081fd12868d2b24197b2168a967aa3
Razafiarison Tafita <tafita.raza@outlook.com>
2022-04-03 15:47:37 +0300

    Setting up navbar and sideBar

--- src/components/navigations/TheNavBar.vue
+++ src/components/navigations/TheNavBar.vue
@@ -1,27 +1,38 @@
 <template>
-    <div class="navbar navbar-dark d-flex justify-content-end p-2 m-0">
-        <div style="min-width: 10rem;">
-            <img 
-                src="../../assets/avatar.png"  
-                alt="user avatar"
-                width="42"
-                class="rounded-circle"/>
-            <span 
-                class="username text-start" 
-                v-text="user.name"
-            ></span>   
-        </div>
-    </div>
-    <div class="navbar navbar-light shadow-sm">
-        <div class="container-fluid">
-            <span class="navbar-brand py-2 m-0 h1">
-                {{title}}
-            </span>
-        </div>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark p-2 m-0">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="#">Vendeurs</a>
+                    <a class="nav-link" href="#">Recettes</a>
+                </div>
+            </div>
+            
+            <div class="dropdown" style="min-width: 10rem;">
+                <div id="dropdownMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img 
+                        src="../../assets/avatar.png"  
+                        alt="user avatar"
+                        width="42"
+                        class="rounded-circle"/>
+                    <span 
+                        class="username text-start" 
+                        v-text="user.name"
+                    ></span>
+                </div>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu">
+                    <li class="dropdown-item" @click="logout">Déconnexion</li>
+                </ul>
+            </div>
+    </nav>
+    <div class="navbar navbar-light shadow-sm p-3 m-0"></div>
 </template>
 
 <script>
+import { mapMutations, mapGetters } from 'vuex'
+
 export default {
     props: {
         title: {
@@ -29,17 +40,62 @@ export default {
             default: 'vendeurs'
         }
     },
+
+    computed: {
+        ...mapGetters([
+            'getToken',
+            'getUser'
+        ])
+    },
+
     data () {
         return {
             user: {
                 name: 'tafita raza'
             }
         }
+    },
+
+    methods: {
+        ...mapMutations([
+            'setToken'
+        ]),
+
+        logout () {
+            if(confirm('Voulez vous vraiment vous déconnecter?')) {
+                this.axios.delete(
+                    'logout',
+                    {headers: { 'Authorization': 'Bearer ' + this.getToken }}
+                ).then( () => {
+                    alert('logout successfully')
+                    this.setToken('')
+                    this.$router.push({ name: 'Auth' })
+                }).catch( err => {
+                    alert(err)
+                })
+            }
+        }
+    },
+
+    mounted () {
+        this.user.name = this.getUser
     }
 }
 </script>
 
 <style scoped>
+.active {
+    background: rgba(255, 255, 255, 0.1);
+}
+
+.nav-link {
+    color: #fcfcfc !important;
+}
+
+.dropdown {
+    cursor: pointer;
+}
+
 .username {
     margin-left: 10%;
     font-weight: bolder;
@@ -53,6 +109,7 @@ export default {
 
 .navbar-light {
     background-color: #fcfcfc;
+    height: 3.3rem;
 }
 
 .navbar-brand::first-letter {
--- src/components/navigations/TheSideBar.vue
+++ src/components/navigations/TheSideBar.vue
@@ -1,18 +1,41 @@
 <template>
-    <div class="navbar bg-light p-0 shadow d-flex flex-column justify-content-start">
-        <div class="navbar-title pt-2 d-flex justify-content-center">
-            <i class="bi bi-check-all" style="font-size: 2rem;"></i>
+    <div class="navbar bg-light shadow d-flex flex-column justify-content-start">
+        <div class="navbar-title pt-2 d-flex">
+            <i 
+                class="bi bi-check-all" 
+                style="font-size: 2rem;"
+            ></i>
             <div class="fs-3">
                 <span class="fw-bold">G.</span>Vente
             </div>
         </div>
-        <div class="navbar-item d-flex justify-content-center align-items-center">
+        <div 
+            class="navbar-item fs-5 my-2"
+            v-for="navbarItem in navbarItems" 
+            :key="navbarItem.title"
+            :to="{name: navbarItem.route}">
+            <base-icon
+                :name="navbarItem.icon"
+                color="gray"/>
             <span class="text-capitalize">
-                dashboard
+                {{ ' ' + navbarItem.title }}
             </span>
         </div>
     </div>
 </template>
+
+<script>
+
+export default {
+    data () {
+        return {
+            navbarItems: [
+                {title: 'vendeurs', icon: 'bi-person-badge', route: 'Vendeurs'},
+            ]
+        }
+    }
+}
+</script>
 
 <style scoped>
 .navbar {
@@ -23,9 +46,13 @@
 .navbar-title {
     height: 4.2rem;
     color: #1563c3;
+    width: 100%;
+    padding-left: 1rem;
+    letter-spacing: 0.5em;
 }
 
-.navbar-item {
-    height: 3rem;
+.navbar-item > .bi {
+    margin-left: 0px;
+
 }
 </style>
